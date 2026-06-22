d1ae8597b82570023d353b26f601fbc6744062d2
zodiacc7 <126355039+zodiacc7@users.noreply.github.com>
2026-06-22 20:47:53 +0330

    Update Storable.cs

--- Storable.cs
+++ Storable.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using Mafi;
 using Mafi.Base;
 using Mafi.Core;
@@ -7,28 +7,43 @@ using Mafi.Core.Products;
 
 namespace Storable;
 
-public sealed class Storable : DataOnlyMod {
+public sealed class Storable : DataOnlyMod
+{
     public override string Name => nameof(Storable);
     public override int Version => 1;
 
-    public Storable(CoreMod coreMod, BaseMod baseMod) {}
+    public Storable(
+        ModManifest manifest,
+        CoreMod coreMod,
+        BaseMod baseMod)
+        : base(manifest)
+    {
+    }
 
-    public override void RegisterPrototypes(ProtoRegistrator registrator) {
-        ProductProto.ID[] ids = {Ids.Products.Exhaust};
+    public override void RegisterPrototypes(ProtoRegistrator registrator)
+    {
+        ProductProto.ID[] ids = { Ids.Products.Exhaust };
 
         FieldInfo? fieldInfo = typeof(ProductProto).GetField(
             nameof(ProductProto.IsStorable),
             BindingFlags.Public | BindingFlags.Instance
         );
-        if (fieldInfo is null) {
-            Log.Warning($"Storable: not find {nameof(ProductProto.IsStorable)} field");
-        } else {
-            foreach (ProductProto.ID id in ids) {
-                if (registrator.PrototypesDb.TryGetProto(id, out ProductProto productProto)) {
-                    fieldInfo.SetValue(productProto, true);
-                } else {
-                    Log.Warning($"Storable: not find {nameof(ProductProto.ID)} {id}");
-                }
+
+        if (fieldInfo is null)
+        {
+            Log.Warning("Storable: IsStorable field not found");
+            return;
+        }
+
+        foreach (ProductProto.ID id in ids)
+        {
+            if (registrator.PrototypesDb.TryGetProto(id, out ProductProto productProto))
+            {
+                fieldInfo.SetValue(productProto, true);
+            }
+            else
+            {
+                Log.Warning($"Storable: Product not found {id}");
             }
         }
     }
